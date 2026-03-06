07f083619dffbd8a547fafdb9ee3384eb69271bb
Seif Eldeen <saifeldeenm4@gmail.com>
2026-03-06 21:12:21 +0200

    printed few calculations in both files

--- task1-first.py
+++ task1-first.py
@@ -1,1 +1,4 @@
-print("first commit")
+#print("first commit")
+print(3 + 5)
+print(3 ** 2)
+print(4 - 2)
--- task2-second.py
+++ task2-second.py
@@ -1,1 +1,4 @@
-print("second commit")
+#print("second commit")
+print(int(3 / 1.5))
+print(2 - 1)
+print(3 ** 3)
